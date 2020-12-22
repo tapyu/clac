75b2b1678d16119006c16a7fac3d1c06045ee5ed
Tapyu <rubem.engenharia@gmail.com>
2020-12-22 14:51:20 -0300

    some steps further on rpa

--- rpa.py
+++ rpa.py
@@ -1,4 +1,4 @@
-import subprocess, pyautogui, time, os
+import subprocess, pyautogui, time, glob, os
 
 def add_word(word, option):
     """
@@ -6,8 +6,19 @@ def add_word(word, option):
     """
     subprocess.Popen(r'C:\Program Files\Anki\anki.exe') # opening the anki program
     time.sleep(2)
-    pyautogui.press('a') # opening the add window
+    pyautogui.press('a') # opening the add window - in the front area
 
-    os.chdir(os.getcwd() + f'\\words\\{word}\\meaning_{option}') # switching the folder to the corrrect one
+    n_example = len(glob.glob(f'./words/{word}/meaning_{option}/example[0-9].txt')) # numbers of examples
+
+    time.sleep(2)
+    for example_number in range(n_example):
+        with open(f'./words/{word}/meaning_{option}/example{example_number}.txt', 'r') as file:
+            pyautogui.write('Example:' + next(file)) # write the example
+            pyautogui.press('f3') # attach picture/audio/video
+            pyautogui.press('alt', 'd') # path insert mode
+            pyautogui.write(os.getcwd() + 'something') # TODO
+            if example_number == 0: # implies that the example was already given in the front area, move to back area
+                time.sleep(2)
+                pyautogui.press('tab')
 
 add_word('bait', 2)
